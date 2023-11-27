a17a8baaee1a3e2f7d033ae14927f2c40e413819
djtest123 <darenj@gmail.com>
2023-11-27 11:07:28 -0500

    GCP: remove scripts

--- .bak/delete_cluster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# gcloud auth application-default login
-gcloud container clusters get-credentials my-cluster --region us-central1
-gcloud container clusters delete my-cluster --region us-central1
