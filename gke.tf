ed65369d310dacdadee22e0d097205b599b70b99
daren <darenjacobs@msn.com>
2024-07-19 17:09:48 -0400

    Helm deploy

--- gke.tf
+++ gke.tf
@@ -5,21 +5,14 @@ provider "google" {
 }
 
 resource "google_container_cluster" "my_cluster" {
-  name     = "my-cluster"
-  location = "us-central1"
-  # deletion_protection = false # Set deletion protection to false
+  name                = "my-cluster"
+  location            = "us-central1"
 
-  # Enabling Autopilot for this cluster
   enable_autopilot = true
-
-  # initial_node_count = 1
-
-  # node_config {
-  #   machine_type = "n1-standard-1"
-  # }
 }
 
 data "google_client_config" "provider" {}
+
 data "google_container_cluster" "my_cluster" {
   name       = "my-cluster"
   location   = "us-central1"
@@ -35,61 +28,6 @@ provider "kubernetes" {
     command     = "gke-gcloud-auth-plugin"
   }
 }
-
-# resource "kubernetes_deployment" "my_app" {
-#   metadata {
-#     name = "my-app"
-#   }
-#
-#   spec {
-#     replicas = 1
-#
-#     selector {
-#       match_labels = {
-#         app = "my-app"
-#       }
-#     }
-#
-#     template {
-#       metadata {
-#         labels = {
-#           app = "my-app"
-#         }
-#       }
-#
-#       spec {
-#         container {
-#           name  = "my-app"
-#           image = "darenjacobs/flask-app:latest"
-#           port {
-#             container_port = 8080
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-#
-
-# resource "kubernetes_service" "my_app_service" {
-#   metadata {
-#     name = "my-app-service"
-#   }
-#
-#   spec {
-#     selector = {
-#       app = "my-app"
-#     }
-#
-#     type = "LoadBalancer"
-#     port {
-#       protocol    = "TCP"
-#       port        = 80
-#       target_port = 8080
-#     }
-#   }
-# }
-
 
 provider "helm" {
   kubernetes {
@@ -114,16 +52,14 @@ resource "helm_release" "flask_app" {
   ]
 }
 
-# some form of automated tests to validate the environment.
-# New feature in Terraform:https://github.com/hashicorp/terraform/releases/tag/v1.5.0  https://developer.hashicorp.com/terraform/language/checks
+# Automated health check
+check "health_check" {
+  data "http" "my_app_service" {
+    url = "http://${helm_release.flask_app.status.load_balancer.ingress[0].ip}/"
+  }
 
-# check "health_check" {
-#   data "http" "my_app_service" {
-#     url = "http://${kubernetes_service.my_app_service.status.0.load_balancer.0.ingress.0.ip}/"
-#   }
-#
-#   assert {
-#     condition     = data.http.my_app_service.status_code == 200
-#     error_message = "ERROR: returned an unhealthy status code"
-#   }
-# }
+  assert {
+    condition     = data.http.my_app_service.status_code == 200
+    error_message = "ERROR: returned an unhealthy status code"
+  }
+}
