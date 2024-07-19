a44923aef1ff7768cf22beb4e1b84a85b158fae2
daren <darenjacobs@msn.com>
2024-07-19 15:31:32 -0400

    update

--- gke.tf
+++ gke.tf
@@ -118,7 +118,7 @@ resource "helm_release" "flask_app" {
 # New feature in Terraform:https://github.com/hashicorp/terraform/releases/tag/v1.5.0  https://developer.hashicorp.com/terraform/language/checks
 
 check "health_check" {
-  data "http" "my_app" {
+  data "http" "flask_app" {
     url = "http://${kubernetes_service.my_app_service.status.0.load_balancer.0.ingress.0.ip}/"
   }
 
