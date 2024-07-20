33bc0634fad7398c7bf7687c6ba58074d92e91c4
daren <darenjacobs@msn.com>
2024-07-20 14:54:11 -0400

    health check

--- gke.tf
+++ gke.tf
@@ -53,7 +53,7 @@ resource "helm_release" "flask_app" {
 # Automated health check
 check "health_check" {
   data "http" "my_app_service" {
-    url = "http://${helm_release.flask_app.status.load_balancer.ingress[0].ip}/"
+    url = "http://${helm_release.flask_app.status.0.load_balancer.0.ingress.0.ip}/"
   }
 
   assert {
@@ -61,3 +61,26 @@ check "health_check" {
     error_message = "ERROR: returned an unhealthy status code"
   }
 }
+
+# resource "helm_release" "flask_app" {
+#   name       = "my-app"
+#   chart      = "${path.module}/flask-app"
+#   namespace  = "default"
+#   values     = [
+#     file("${path.module}/flask-app/values.yaml")
+#   ]
+#
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       # Retrieve the external IP address of the service
+#       SERVICE_IP=$(kubectl get svc my-app-flask-app -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+#       # Perform the health check
+#       STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://${SERVICE_IP})
+#       if [ "${STATUS_CODE}" -ne 200 ]; then
+#         echo "ERROR: returned an unhealthy status code"
+#         exit 1
+#       fi
+#     EOT
+#     interpreter = ["/bin/bash", "-c"]
+#   }
+# }
