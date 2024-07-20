309c13f800d0fa2b0de536306a7a4933839d3633
daren <darenjacobs@msn.com>
2024-07-20 15:00:18 -0400

    using file

--- gke.tf
+++ gke.tf
@@ -50,37 +50,37 @@ resource "helm_release" "flask_app" {
   ]
 }
 
-# Automated health check
-check "health_check" {
-  data "http" "my_app_service" {
-    url = "http://${helm_release.flask_app.status.0.load_balancer.0.ingress.0.ip}/"
-  }
-
-  assert {
-    condition     = data.http.my_app_service.status_code == 200
-    error_message = "ERROR: returned an unhealthy status code"
-  }
-}
-
-# resource "helm_release" "flask_app" {
-#   name       = "my-app"
-#   chart      = "${path.module}/flask-app"
-#   namespace  = "default"
-#   values     = [
-#     file("${path.module}/flask-app/values.yaml")
-#   ]
+# # Automated health check
+# check "health_check" {
+#   data "http" "my_app_service" {
+#     url = "http://${helm_release.flask_app.status.0.load_balancer.0.ingress.0.ip}/"
+#   }
 #
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       # Retrieve the external IP address of the service
-#       SERVICE_IP=$(kubectl get svc my-app-flask-app -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-#       # Perform the health check
-#       STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://${SERVICE_IP})
-#       if [ "${STATUS_CODE}" -ne 200 ]; then
-#         echo "ERROR: returned an unhealthy status code"
-#         exit 1
-#       fi
-#     EOT
-#     interpreter = ["/bin/bash", "-c"]
+#   assert {
+#     condition     = data.http.my_app_service.status_code == 200
+#     error_message = "ERROR: returned an unhealthy status code"
 #   }
 # }
+
+resource "helm_release" "flask_app" {
+  name       = "my-app"
+  chart      = "${path.module}/flask-app"
+  namespace  = "default"
+  values     = [
+    file("${path.module}/flask-app/values.yaml")
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      # Retrieve the external IP address of the service
+      SERVICE_IP=$(kubectl get svc my-app-flask-app -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+      # Perform the health check
+      STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://${SERVICE_IP})
+      if [ "${STATUS_CODE}" -ne 200 ]; then
+        echo "ERROR: returned an unhealthy status code"
+        exit 1
+      fi
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
