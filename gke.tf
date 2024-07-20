626db534257a39ffa91f4bfdf86b332cad21f5b9
daren <darenjacobs@msn.com>
2024-07-20 12:03:20 -0400

    specify kubeconfig

--- gke.tf
+++ gke.tf
@@ -26,25 +26,26 @@ provider "kubernetes" {
     args        = []
     command     = "gke-gcloud-auth-plugin"
   }
+  config_path = "~/.kube/config"  # Explicitly set the kubeconfig path
 }
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
-#     cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
-#     exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       args        = []
-#       command     = "gke-gcloud-auth-plugin"
-#     }
-#   }
-# }
-#
-# resource "helm_release" "flask_app" {
-#   name       = "flask-app"
-#   chart      = "${path.module}/flask-app"
-#   namespace  = "default"
-#   values     = [
-#     file("${path.module}/flask-app/values.yaml")
-#   ]
-# }
+provider "helm" {
+  kubernetes {
+    host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
+    cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = []
+      command     = "gke-gcloud-auth-plugin"
+    }
+  }
+}
+
+resource "helm_release" "flask_app" {
+  name       = "my-app"
+  chart      = "${path.module}/flask-app"
+  namespace  = "default"
+  values     = [
+    file("${path.module}/flask-app/values.yaml")
+  ]
+}
