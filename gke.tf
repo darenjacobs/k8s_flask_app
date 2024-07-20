dd5ba82e643bbc11bdb0d8e45b5a57384e24ffbc
daren <darenjacobs@msn.com>
2024-07-20 11:41:11 -0400

    terraform destroy is not working???

--- gke.tf
+++ gke.tf
@@ -28,23 +28,23 @@ provider "kubernetes" {
   }
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
-    cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = []
-      command     = "gke-gcloud-auth-plugin"
-    }
-  }
-}
-
-resource "helm_release" "flask_app" {
-  name       = "flask-app"
-  chart      = "${path.module}/flask-app"
-  namespace  = "default"
-  values     = [
-    file("${path.module}/flask-app/values.yaml")
-  ]
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
+#     cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       args        = []
+#       command     = "gke-gcloud-auth-plugin"
+#     }
+#   }
+# }
+#
+# resource "helm_release" "flask_app" {
+#   name       = "flask-app"
+#   chart      = "${path.module}/flask-app"
+#   namespace  = "default"
+#   values     = [
+#     file("${path.module}/flask-app/values.yaml")
+#   ]
+# }
